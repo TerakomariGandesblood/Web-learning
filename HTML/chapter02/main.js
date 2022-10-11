1fdb451d66fc928497fb66d7fde7ce28cf911cd2
Terakomari <im.terakomari@icloud.com>
2022-10-11 20:20:14 +0800

    feat: learning HTML

---
+++ HTML/chapter02/main.js
@@ -0,0 +1,5 @@
+let button = document.querySelector("button");
+button.onclick = function () {
+  let video = document.querySelector("video");
+  video.load();
+};
--- example-site/scripts/main.js
+++ example-site/scripts/main.js
@@ -1,3 +1,5 @@
+// JavaScript 为网站提供动态交互特性
+
 // === -> 相等
 // !== -> 不相等
 
@@ -17,6 +19,8 @@ let h1 = document.querySelector("h1");
 
 function setUserName() {
   let name = prompt("请输入你的名字");
+  // null 或者空
+  // 取消提示对话框后值将设置为 null，这是 JavaScript 中的一个特殊值，表示引用不存在
   if (!name) {
     setUserName();
   }
@@ -24,14 +28,14 @@ function setUserName() {
   // Web Storage API
   // see https://developer.mozilla.org/zh-CN/docs/Web/API/Web_Storage_API
   localStorage.setItem("name", name);
-  h1.textContent = "Mozilla 酷毙了, " + name;
+  h1.textContent = "Mozilla 酷毙了，" + name;
 }
 
 if (!localStorage.getItem("name")) {
   setUserName();
 } else {
   let name = localStorage.getItem("name");
-  h1.textContent = "Mozilla 酷毙了, " + name;
+  h1.textContent = "Mozilla 酷毙了，" + name;
 }
 
 // 事件
@@ -39,3 +43,8 @@ if (!localStorage.getItem("name")) {
 document.querySelector("button").onclick = function () {
   setUserName();
 };
+
+// 箭头函数
+// document.querySelector("button").onclick = () => {
+//   setUserName();
+// };
